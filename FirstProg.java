4248d356432ecffa9752896ae7908a47e1590eb2
Aman <amanth688@gmail.com>
2023-09-12 22:00:58 +0530

    first program

---
+++ FirstProg.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class FirstProg{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+
+
+
+    }
+}
